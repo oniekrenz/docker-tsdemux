19ce9fcada13f5620fdf515349eb495f57da3819
oniekrenz <oliver@niekrenz.de>
2016-03-20 21:38:55 +0100

    First version

---
+++ Demux.py
@@ -0,0 +1,90 @@
+import json
+import os
+import re
+import subprocess
+import time
+import sys
+
+ts_pattern = re.compile('^.*\.(\d{4}-\d{2}-\d{2}\.\d{2}-\d{2})\.ts$')
+vdr_pattern = re.compile('^(\d{4}-\d{2}-\d{2}\.\d{2}.\d{2})\.\d{2}\.\d{2}\.rec$')
+
+
+def get_jobs(path, title):
+    jobs = []
+    basename = os.path.basename(path)
+    if os.path.isfile(path):
+        match = ts_pattern.match(basename)
+        if match:
+            timestamp = time.strptime(match.group(1), '%Y-%m-%d.%H-%M')
+            job = {'title': title, 'files': [path], 'timestamp': timestamp}
+            jobs.append(job)
+    else:
+        match = vdr_pattern.match(basename)
+        if match:
+            files = [os.path.join(path, file) for file in os.listdir(path) if re.match('\d+\.vdr$', file)]
+            files.sort()
+            timestamp = time.strptime(match.group(1), '%Y-%m-%d.%H.%M')
+            job = {'title': title, 'files': files, 'timestamp': timestamp}
+            jobs.append(job)
+        else:
+            for file in os.listdir(path):
+                subdir = os.path.join(path, file)
+                jobs.extend(get_jobs(subdir, title))
+    return jobs
+
+
+def create_jobs(job_definition, job_definition_dir):
+    rootdir = job_definition['root'] if 'root' in job_definition else job_definition_dir
+    jobs = []
+    if 'entries' in job_definition:
+        for entry in job_definition['entries']:
+            entrydir = entry['dir'] if 'dir' in entry else ''
+            title = entry['title'] if 'title' in entry else ''
+            jobs.extend(get_jobs(os.path.join(rootdir, entrydir), title))
+    return jobs
+
+
+def add_language_to_audio_files(dir, name):
+    pids = {}
+    log_file_name = os.path.join(dir, name + '_log.txt')
+    with open(log_file_name, 'r') as log_file:
+        context = 'none'
+        pid = ''
+        print "reading log " + log_file_name
+        for line in log_file:
+            pid_match = re.match('^PID: 0x(\w+)\{(\w+)\}.*', line)
+            output_match = re.match('^\+\+> .*?: PID 0x(\w+) .*', line)
+            if (pid_match):
+                pids[pid_match.group(1)] = pid_match.group(2)
+            elif output_match:
+                context = 'output'
+                pid = output_match.group(1)
+            elif line == '':
+                context = 'none'
+            if (context == 'output'):
+                out_match = re.match('---> new File: \'(.*?)\'', line)
+                if (out_match and pid in pids):
+                    lang = pids[pid]
+                    new_file = re.sub('(\.\w+)$', ' ' + lang + '\\1', out_match.group(1))
+                    print "move " + out_match.group(1) + ' -> ' + new_file
+                    os.rename(out_match.group(1), new_file)
+
+
+def process_jobs(jobs, root_dir):
+    dest_dir = root_dir
+    for job in jobs:
+        name = job['title'] + time.strftime('_%Y-%m-%d_%H-%M', job['timestamp'])
+        demux_cmd = ['java', '-jar', '/opt/Project-X_0.91.0/ProjectX.jar']
+        demux_cmd.extend(['-out', dest_dir, '-name', name, '-demux'])
+        demux_cmd.extend(job['files'])
+        #subprocess.call(demux_cmd)
+        #add_language_to_audio_files(dest_dir, name)
+        print "foo"
+
+
+job_definition_file_name = sys.argv[1]
+with open(job_definition_file_name, 'r') as job_definition_file:
+    job_definition = json.load(job_definition_file)
+    root_dir = os.path.dirname(job_definition_file_name)
+    jobs = create_jobs(job_definition, root_dir)
+    process_jobs(jobs, root_dir)
---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM debian
+MAINTAINER Oliver Niekrenz <oliver@niekrenz.de>
+RUN apt-get update && apt-get install -y \
+  python \
+  default-jre \
+  unzip \
+  wget
+RUN wget -q -O ProjectX.zip https://sourceforge.net/projects/project-x/files/project-x/ProjectX_0.91.0.00/ProjectX_0.91.0.zip/download \
+  && unzip ProjectX.zip -d /opt
+RUN mkdir /opt/TsDemuxer
+COPY Demux.py /opt/TsDemuxer
+ENTRYPOINT ["python", "/opt/TsDemuxer/Demux.py"]
+
